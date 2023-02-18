f5b81bb7c04c60931c75ea765e1876dfa7a9b2bb
Bokang-m <bmokochoa@gmail.com>
2023-02-17 17:16:23 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+
+int main(void)
+
+{
+char abc;
+
+for (abc = 'z' ; abc >= 'a'; abc--)
+{
+putchar(abc);
+}
+
+putchar('\n');
+
+return (0);
+
+}
+
