3bd06969830191f14cdd604e57c9d9452c48ce2c
Bokang-m <bmokochoa@gmail.com>
2023-02-17 18:06:37 -0800

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+int a;
+
+
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+
+if (a == '9')
+{
+break;
+}
+
+putchar(',');
+putchar (' ');
+}
+
+putchar('\n');
+
+return (0);
+
+}
