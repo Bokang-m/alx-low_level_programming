d7728c7da420fda7785a7f921c2e7cf870162375
Bokang-m <bmokochoa@gmail.com>
2023-02-21 17:39:16 -0800

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,24 +6,24 @@
  */
 void jack_bauer(void)
 {
-	int a;
-	int b;
+	int i;
+	int j;
 
-	a = 0;
+	i = 0;
 
-	while (a < 24)
+	while (i < 24)
 	{
-		b = 0;
-		while (b < 60)
+		j = 0;
+		while (j < 60)
 		{
-			_putchar((a / 10) + '0');
-			_putchar((a % 10) + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((b / 10) + '0');
-			_putchar((b % 10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
-			a++;
+			j++;
 		}
-		b++;
+		i++;
 	}
 }
