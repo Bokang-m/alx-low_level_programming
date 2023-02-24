62e6480dcc8938290baff40c0a70aa463dd0250f
Bokang-m <bmokochoa@gmail.com>
2023-02-23 18:19:54 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - fizbuzz
+* Return: 0
+*/
+
+int main(void)
+{
+int q;
+
+for (q = 1; q < 100; q++)
+{
+if (q % 3 == 0 && q % 5 == 0)
+printf("FizzBuzz ");
+else if (q % 3 == 0)
+printf("Fizz ");
+else if (q % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", q);
+}
+printf("Buzz\n");
+return (0);
+}
