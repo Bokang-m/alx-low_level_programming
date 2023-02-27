cfd20598573d5f13768ea2af023ec481b8c258ba
Bokang-m <bmokochoa@gmail.com>
2023-02-27 15:09:44 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints a string to stdout
+ * @str: string to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+int a = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+a++;
+}
+_putchar(\'n');
+}
