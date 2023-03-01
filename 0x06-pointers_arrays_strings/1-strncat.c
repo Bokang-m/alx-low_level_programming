cbd2c0b41da0ec3309af5279e0628c73132acb5b
Bokang-m <bmokochoa@gmail.com>
2023-03-01 14:20:26 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: Number of bytes
+ *
+ * Return: A pointer to the resulting sting @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int dest_len = 0, i = 0;
+
+/* Find the length of the destination string */
+while (dest[dest_len] != '\0')
+dest_len++;
+
+/* Concatenate at most n bytes from src */
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[dest_len + i] = src[i];
+
+/* Append the source string to the destination sting */
+dest[dest_len + i] = '\0';
+
+/*Add the terminating null byte*/
+return (dest);
+}
