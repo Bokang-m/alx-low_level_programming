a58e8a09a9cca0277a63cba34edca7a5be94a53f
Bokang-m <bmokochoa@gmail.com>
2023-03-02 18:10:26 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,11 +9,11 @@ void print_numbers(void)
 {
 int i;
 
-i = 0;
-while (i <= 9)
-{
-_putchar(i + '0');
-i++;
-}
-_putchar('\n');
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}	
+	_putchar('\n');
 }
