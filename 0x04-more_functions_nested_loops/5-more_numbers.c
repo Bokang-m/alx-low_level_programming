5fc1f6dc7d20de21358a1546fbff11b169c67077
Bokang-m <bmokochoa@gmail.com>
2023-03-02 18:16:56 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,23 +6,23 @@
 
 void more_numbers(void)
 {
-int a, b;
+	int a, b;
 
-a = 0;
+	a = 0;
 
-while (a < 10)
-{
-for (b = 0; b < 15; b++)
-{
-if (b >= 10)
-{
-_putchar((b / 10) + 48);
-}
-_putchar((b % 10) + 48);
-}
+	while (a < 10)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar((b / 10) + 48);
+			}
+			_putchar((b % 10) + 48);
+		}
 
-_putchar('\n');
+	_putchar('\n');
 
-a++;
-}
+	a++;
+	}
 }
