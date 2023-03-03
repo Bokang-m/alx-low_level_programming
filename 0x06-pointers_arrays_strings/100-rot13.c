463ed0fbecf73878eb09de1d625f393f7279ea7f
Bokang-m <bmokochoa@gmail.com>
2023-03-02 16:36:38 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,21 +9,21 @@
  */
 char *rot13(char *s)
 {
-char *str = s;
-int i, j;
-char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *str = s;
+	int i, j;
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 for (i = 0; str[i] != '\0'; i++)
 {
-for (j = 0; letters[j] != '\0'; j++)
-{
-if (str[i] == letters[j])
-{
-str[i] = rot[j];
-break;
-}
-}
+	for (j = 0; letters[j] != '\0'; j++)
+	{
+		if (str[i] == letters[j])
+		{
+			str[i] = rot[j];
+			break;
+		}
+	}
 }
 return (s);
 }
