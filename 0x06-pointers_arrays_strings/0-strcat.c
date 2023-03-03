dcadc09ae1b0317e033b3ef0bcb4c1a1ea1670a1
Bokang-m <bmokochoa@gmail.com>
2023-03-02 16:42:11 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,18 +10,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int dest_len = 0, i;
+	int dest_len = 0, i;
 
-/* Find the length of the destination string */
-while (dest[dest_len] != '\0')
-dest_len++;
+	/* Find the length of the destination string */
+	while (dest[dest_len] != '\0')
+	dest_len++;
 
-for (i = 0; src[i] != '\0'; i++)
-dest[dest_len + i] = src[i];
+	for (i = 0; src[i] != '\0'; i++)
+	dest[dest_len + i] = src[i];
 
-/* Append the source string to the destination sting */
-dest[dest_len + i] = '\0';
+	/* Append the source string to the destination sting */
+	dest[dest_len + i] = '\0';
 
-/*Add the terminating null byte*/
-return (dest);
+	/*Add the terminating null byte*/
+	return (dest);
 }
