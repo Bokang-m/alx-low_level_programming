92a7415409ab055634d1cdec4f85ff65340faa0f
Bokang-m <bmokochoa@gmail.com>
2023-03-02 18:24:16 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,20 +6,20 @@
  */
 void print_square(int size)
 {
-int height, width;
+	int height, width;
 
-if (size > 0)
-{
-for (height = 0; height < size; height++)
-{
-for (width = 0; width < size; width++)
-_putchar('#');
+	if (size > 0)
+	{
+		for (height = 0; height < size; height++)
+		{
+			for (width = 0; width < size; width++)
+			_putchar('#');
 
-if (height == size - 1)
-continue;
-_putchar('\n');
-}
-}
+			if (height == size - 1)
+			continue;
+			_putchar('\n');
+		}
+	}
 
-_putchar('\n');
+	_putchar('\n');
 }
