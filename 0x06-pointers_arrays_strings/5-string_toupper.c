6aad73c427d5531c7619b89bed3edbef1188a292
Bokang-m <bmokochoa@gmail.com>
2023-03-02 16:57:16 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,16 +6,16 @@
  */
 char *string_toupper(char *str)
 {
-char *ptr = str;
+	char *ptr = str;
 
-while (*ptr)
-{
-if (*ptr >= 'a' && *ptr <= 'z')
-{
-*ptr = *ptr - 32;
-}
-ptr++;
-}
-return (str);
+	while (*ptr)
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - 32;
+		}
+		ptr++;
+	}
+	return (str);
 }
 
