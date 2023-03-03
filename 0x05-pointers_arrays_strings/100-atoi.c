8180deae18e9423a8118af7836bc08901581d69c
Bokang-m <bmokochoa@gmail.com>
2023-03-02 18:00:24 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,19 +9,19 @@
  */
 int _atoi(char *s)
 {
-int sign = 1;
-unsigned int num = 0;
+	int sign = 1;
+	unsigned int num = 0;
 
-while (*s)
+	while (*s)
 	{
-if (*s == '-')
-sign *= -1;
-else if (*s >= '0' && *s <= '9')
-num = num * 10 + (*s - '0');
-else if (num > 0)
-break;
-s++;
-}
-return (num *sign);
+		if (*s == '-')
+		sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+		num = num * 10 + (*s - '0');
+		else if (num > 0)
+		break;
+		s++;
+	}
+	return (num * sign);
 }
 
