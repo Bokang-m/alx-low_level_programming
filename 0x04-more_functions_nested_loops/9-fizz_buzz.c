19b1c5159af6f245a4494b3eab03235ecd6cf023
Bokang-m <bmokochoa@gmail.com>
2023-03-02 18:26:20 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,19 +7,19 @@
 
 int main(void)
 {
-int q;
+	int q;
 
-for (q = 1; q < 100; q++)
-{
-if (q % 3 == 0 && q % 5 == 0)
-printf("FizzBuzz ");
-else if (q % 3 == 0)
-printf("Fizz ");
-else if (q % 5 == 0)
-printf("Buzz ");
-else
-printf("%d ", q);
-}
-printf("Buzz\n");
-return (0);
+	for (q = 1; q < 100; q++)
+	{
+		if (q % 3 == 0 && q % 5 == 0)
+		printf("FizzBuzz ");
+		else if (q % 3 == 0)
+		printf("Fizz ");
+		else if (q % 5 == 0)
+		printf("Buzz ");
+		else
+		printf("%d ", q);
+	}
+	printf("Buzz\n");
+	return (0);
 }
