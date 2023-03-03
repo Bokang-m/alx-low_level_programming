3294bdb51162b3ef033beed78fe8c1c0db1925c3
Bokang-m <bmokochoa@gmail.com>
2023-03-02 17:45:48 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,18 +6,18 @@
  */
 void rev_string(char *s)
 {
-int len = 0, i = 0;
-char temp;
+	int len = 0, i = 0;
+	char temp;
 
-/* find the length of the string */
-while (s[len] != '\0')
-len++;
+	/* find the length of the string */
+	while (s[len] != '\0')
+	len++;
 
-/* swap characters from start and end */
-for (i = 0; i < len / 2; i++)
-{
-temp = s[i];
-s[i] = s[len - i - 1];
-s[len - i - 1] = temp;
-}
+	/* swap characters from start and end */
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
 }
