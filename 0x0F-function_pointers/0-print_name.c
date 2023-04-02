a73e7b0f12f665039ba0479fd30d7cbefec9b221
Bokang-m <bmokochoa@gmail.com>
2023-04-01 19:30:56 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,11 @@
  * print_name - prints a name
  * @name: the name of a person
  * @f: pointer to the assigned function
- * Return: Nothing
+ * Return: Always zero
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
+	if (!name || !f)
+	return;
 	f(name);
-
 }
